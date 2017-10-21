9f2030cd077ac2695aa72e587e079cfde67d7fb1
vamosk <krisztivamos@gmail.com>
2017-10-21 21:41:06 +0200

    update translate.js

--- src/translate.js
+++ src/translate.js
@@ -8,7 +8,7 @@ module.exports.translateConfig = function translateConfig($translateProvider, tm
       prefix: 'locales/',
       suffix: '.json'
     })
-    .registerAvailableLanguageKeys(['en', 'fr', 'de', 'sl'], {
+    .registerAvailableLanguageKeys(['en', 'fr', 'de', 'sl', 'hu'], {
       'en_US': 'en',
       'en_UK': 'en',
       'en-US': 'en',
